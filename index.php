e14bbb075097a22533841a5332c3d78447da572d
Grigoriy Kosarev <grigoriykosarev@gmail.com>
2017-09-29 11:44:11 +0300

    commit1

---
+++ index.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8">
+<?php
+    const COUNTRY_NAME = "Україна";
+
+    $first_name = 'Григорій';
+    $last_name = 'Косарєв';
+
+?>
+
+<h3>Привіт, <?php echo $first_name?>  <?php echo $last_name?>
+    . Ви проживаєте в країні <?php echo COUNTRY_NAME ?>
+</h3>
+
+<p>Поточна дата:  <?php echo date("Y-m-d") ?></p>
+
+
